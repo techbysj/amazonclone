aa8402a30a878204cce62e5fe5befa2176bcbc4e
techbysj <jsabood19@gmail.com>
2025-03-20 10:46:33 +0000

    fixing cart quantity

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -58,10 +58,23 @@ document.querySelector(".js-products-grid").innerHTML = productsHTML;
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
     const productName = button.dataset.productName;
-    cart.push({
-      productName: productName,
-      quantity: 1,
+
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if (productName === item.productName) {
+        matchingItem = item;
+      }
     });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({
+        productName: productName,
+        quantity: 1,
+      });
+    }
     console.log(cart);
   });
 });
